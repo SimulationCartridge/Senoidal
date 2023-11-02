7d601e9fc6f4c760f9ed1c463b83e8d3bf7814c2
SimulationCartridge <122331289+SimulationCartridge@users.noreply.github.com>
2023-11-01 21:25:26 -0600

    Update BlogDetails.js

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { client } from '../../client';
-import {useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import marked from 'marked';
 
 const BlogDetails = () => {
   const [singleBlogPost, setSingleBlogPost] = useState([]);
   let { id } = useParams();
-
 
   useEffect(() => {
     const getEntryById = async () => {
@@ -20,17 +20,18 @@ const BlogDetails = () => {
     getEntryById()
   }, [id])
 
-  return (
-<div class="container-detail">
-  <section class="blog-post">
-    <img className="post-image-full"src={singleBlogPost?.fields?.images?.fields?.file?.url} title="" alt={singleBlogPost?.fields?.titles}  />
-    <h2>{singleBlogPost?.fields?.titles}</h2>
-    <div class="content">
-      <ReactMarkdown>{singleBlogPost?.fields?.content}</ReactMarkdown>
-    </div>
-  </section>
-</div>
+  const renderMarkdown = (content) => {
+    return { __html: marked(content) };
+  }
 
+  return (
+    <div className="container-detail">
+      <section className="blog-post">
+        <img className="post-image-full" src={singleBlogPost?.fields?.images?.fields?.file?.url} title="" alt={singleBlogPost?.fields?.titles} />
+        <h2>{singleBlogPost?.fields?.titles}</h2>
+        <div className="content" dangerouslySetInnerHTML={renderMarkdown(singleBlogPost?.fields?.content)} />
+      </section>
+    </div>
   );
 };
 
