5780c9fc13eb2293c0551cb116b41eb96b8ffa3c
SimulationCartridge <122331289+SimulationCartridge@users.noreply.github.com>
2023-11-01 21:36:04 -0600

    Update BlogDetails.js

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { client } from '../../client';
 import { useParams } from 'react-router-dom';
-import * as marked from 'marked';
 
 const BlogDetails = () => {
   const [singleBlogPost, setSingleBlogPost] = useState([]);
@@ -20,16 +19,14 @@ const BlogDetails = () => {
     getEntryById()
   }, [id])
 
-  const renderMarkdown = (content) => {
-    return { __html: marked(content) };
-  }
-
   return (
-    <div className="container-detail">
-      <section className="blog-post">
-        <img className="post-image-full" src={singleBlogPost?.fields?.images?.fields?.file?.url} title="" alt={singleBlogPost?.fields?.titles} />
+    <div class="container-detail">
+      <section class="blog-post">
+        <img className="post-image-full" src={singleBlogPost?.fields?.images?.fields?.file?.url} title="" alt={singleBlogPost?.fields?.titles}  />
         <h2>{singleBlogPost?.fields?.titles}</h2>
-        <div className="content" dangerouslySetInnerHTML={renderMarkdown(singleBlogPost?.fields?.content)} />
+        <div class="content">
+          <p className='texto-detail'>{singleBlogPost?.fields?.content}</p>
+        </div>
       </section>
     </div>
   );
